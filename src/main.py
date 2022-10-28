670d9390740a826350ea11be5243bc9224d8b98c
MrTaco9001 <mr.taco9001@gmail.com>
2022-10-28 01:10:45 -0400

    made rpm guage into a class

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
 import sys
 from math import cos, pi, sin, atan
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QFrame, QWidget
-from PySide6.QtGui import QCursor, QTransform, QPainter, QFont
-from PySide6.QtCore import Qt
+from PySide6.QtGui import QCursor, QTransform, QPainter, QFont, QTextOption
+from PySide6.QtCore import Qt, QRect
 
 screen_size = [1920, 720]
 rpm_params = {
 	"min": 0,
 	"max": 8,
 	"redline": 6.7,
-	"sections": 10
+	"sections": 14
 }
 
 
@@ -37,73 +37,105 @@ class Line(QWidget):
 		painter.end()
 
 
-class main_window(QMainWindow):
+class Label(QWidget):
 
-	def __init__(self):
-		super().__init__()
+	def __init__(self, parent, text, position, rotation, color, font=None):
+		super().__init__(parent)
+		self.resize(parent.frameGeometry().width(), parent.frameGeometry().height())
+		self.position = position
+		self.rotation = rotation
+		self.text = text
+		self.color = color
+		self.fontObj = font
 
-		self.setWindowTitle("Digital Cluster")
-		self.setFixedSize(screen_size[0], screen_size[1])
-		#self.showFullScreen()
-		self.show()
-		self.setFocus()
+	def paintEvent(self, event):
+		painter = QPainter()
+		painter.begin(self)
+		painter.setBackgroundMode(Qt.TransparentMode)
+		painter.setRenderHint(QPainter.Antialiasing)
+		painter.setPen(self.color)
 
-		self.rpm_labels = []
-		self.make_left_guage(
-			rpm_params["min"], rpm_params["max"], rpm_params["redline"], rpm_params["sections"])
+		if self.fontObj != None:
+			painter.setFont(self.fontObj)
 
-	def make_left_guage(self, min, max, redline, sections):
+		painter.drawText(*self.position, self.text)
+
+
+		painter.rotate(self.rotation)
+		painter.end()
+
+
+class RPMGuage(QWidget):
+
+	def __init__(self, parent, min, max, redline, sections):
+		super().__init__(parent)
+
+		self.resize(500, 500)
+
 		frame = QFrame(self)
 		frame.setStyleSheet("background-color: black")
 		frame.resize(500, 500)
 		frame.show()
 
-		label_size = [15, 15]
 		rad_range = 2 * pi - pi / 2
 		rad_offset = pi / 2 + (2 * pi - rad_range) / 2
 		rad_step = rad_range / (max)
 		rad_section_step = rad_step / sections
 
-		buffer_radius = 50
-		num_radius = 50
+		buffer_radius = 20
+		num_radius = 60
 		section_radius = 20
+		minor_section_rad_offset = 7
+		middle_section_rad_offset = 43
+		major_section_rad_offset = 38
 
-		num_x_radius = frame.frameGeometry().width() / 2 - label_size[0] / 2 - buffer_radius - num_radius
-		num_y_radius = frame.frameGeometry().height() / 2 - label_size[1] / 2 - buffer_radius - num_radius
+		num_x_radius = frame.frameGeometry().width() / 2 - buffer_radius - num_radius
+		num_y_radius = frame.frameGeometry().height() / 2 - buffer_radius - num_radius
 
-		section_x_radius = frame.frameGeometry().width() / 2 - label_size[0] / 2 - buffer_radius - section_radius
-		section_y_radius = frame.frameGeometry().height() / 2 - label_size[1] / 2 - buffer_radius - section_radius
+		section_x_radius = frame.frameGeometry().width() / 2 - buffer_radius - section_radius
+		section_y_radius = frame.frameGeometry().height() / 2 - buffer_radius - section_radius
 
-		x_rad_offset = frame.frameGeometry().width() / 2 - label_size[0] / 2
-		y_rad_offset = frame.frameGeometry().height() / 2 - label_size[1] / 2
+		x_rad_offset = frame.frameGeometry().width() / 2
+		y_rad_offset = frame.frameGeometry().height() / 2
 
 		color = "white"
 
+		label_font = QFont("Sans-serif", 14)
+
 		for i in range(min, max + 1):
-			label = QLabel(frame, text=f"{i}")
-			label.resize(*label_size)
-			label.setAutoFillBackground(False)
-			label.setStyleSheet(f"color: {color}; background: transparent")
-			label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-			label.move(
-						cos(i * rad_step + rad_offset) * num_x_radius + x_rad_offset,
-						sin(i * rad_step + rad_offset) * num_y_radius + y_rad_offset
-					)
+			label = Label(frame, f"{i}",
+					(
+						cos(i * rad_step + rad_offset) * num_x_radius + x_rad_offset - 4,
+						sin(i * rad_step + rad_offset) * num_y_radius + y_rad_offset + 7
+					),
+					0,
+					color,
+					font=label_font
+				)
 			label.show()
 
 			for z in range(0, sections):
 				x_radius = section_x_radius
 				y_radius = section_y_radius
+				x_inner_radius = x_radius
+				y_inner_radius = y_radius
+
 				if z == 0:
-					x_radius -= 10
-					y_radius -= 10
+					x_inner_radius -= num_radius - major_section_rad_offset
+					y_inner_radius -= num_radius - major_section_rad_offset
+				elif (sections % 2 == 0) and (z == sections/2):
+					x_inner_radius -= num_radius - middle_section_rad_offset
+					y_inner_radius -= num_radius - middle_section_rad_offset
+				else:
+					x_inner_radius -= minor_section_rad_offset
+					y_inner_radius -= minor_section_rad_offset
 
 				line = Line(frame,
 								(
 									cos(i * rad_step + rad_offset + z * rad_section_step) * x_radius + x_rad_offset,
 									sin(i * rad_step + rad_offset + z * rad_section_step) * y_radius + y_rad_offset,
-									cos(i * rad_step + rad_offset + z * rad_section_step) * x_radius + x_rad_offset,
-									sin(i * rad_step + rad_offset + z * rad_section_step) * y_radius + y_rad_offset
+									cos(i * rad_step + rad_offset + z * rad_section_step) * x_inner_radius + x_rad_offset,
+									sin(i * rad_step + rad_offset + z * rad_section_step) * y_inner_radius + y_rad_offset
 								),
 								(0, 0), 0, color)
 
@@ -117,8 +149,31 @@ class main_window(QMainWindow):
 
 				label.show()
 
+
+class MainWindow(QMainWindow):
+
+	def __init__(self):
+		super().__init__()
+
+		self.setWindowTitle("Digital Cluster")
+		self.setFixedSize(screen_size[0], screen_size[1])
+		#self.showFullScreen()
+		self.show()
+		self.setFocus()
+
+		self.rpm_guage = RPMGuage(self, rpm_params["min"], rpm_params["max"], rpm_params["redline"], rpm_params["sections"])
+		self.rpm_guage.show()
+
+
+
+class Application(QApplication):
+
+	def __init__(self):
+		super().__init__([])
+		self.setOverrideCursor(QCursor(Qt.BlankCursor))
+		self.primary_container = MainWindow()
+
+
 if __name__ == "__main__":
-	app = QApplication([])
-	app.setOverrideCursor(QCursor(Qt.BlankCursor))
-	window = main_window()
+	app = Application()
 	sys.exit(app.exec())
