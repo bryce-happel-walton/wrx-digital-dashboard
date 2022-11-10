f744a5b213e76af987edb05a29ebcd7706d53d86
MrTaco9001 <mr.taco9001@gmail.com>
2022-11-10 00:36:21 -0500

    fix subprocess call

--- src/main.py
+++ src/main.py
@@ -191,7 +191,8 @@ if __name__ == "__main__":
         app.primary_container.setFixedSize(screen_size[0], screen_size[1])
 
         try:
-            setup_can = subprocess.run("sudo /sbin/ip link set can0 up type can bitrate 500000", check=True)
+            shutdown_can = subprocess.run(["sudo", "/sbin/ip", "link", "set", "can0", "down"], check=True)
+            setup_can = subprocess.run(["sudo", "/sbin/ip", "link", "set", "can0", "up", "type", "can", "bitrate", "500000"], check=True)
         except:
             print("Could not find PiCan device! Quitting.")
             del app
