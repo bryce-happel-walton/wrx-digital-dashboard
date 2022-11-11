de93ee24b6cb6801c10f2331681f46f29219fc3e
MrTaco9001 <mr.taco9001@gmail.com>
2022-11-10 22:51:57 -0500

    test can reading

--- src/can_handle.py
+++ src/can_handle.py
@@ -9,4 +9,17 @@ class CanApplication():
                                      bitrate=500000)
 
     def printout(self):
-        notifier = can.Notifier(self.bus, [can.Printer()])
+        can.Notifier(self.bus, [can.Printer()])
+
+
+if __name__ == "__main__":
+    import subprocess
+
+    try:
+        shutdown_can = subprocess.run(["sudo", "/sbin/ip", "link", "set", "can0", "down"], check=True)
+        setup_can = subprocess.run(["sudo", "/sbin/ip", "link", "set", "can0", "up", "type", "can", "bitrate", "500000"], check=True)
+    except:
+        print("Could not find PiCan device! Quitting.")
+
+    can_app = CanApplication()
+    can_app.printout()
