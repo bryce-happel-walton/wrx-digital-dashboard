1c06d925f9bbf33f8513fa5c37b739264f545fac
MrTaco9001 <mr.taco9001@gmail.com>
2022-11-11 20:44:15 -0500

    added turn signal support and test cases

--- src/can_data.py
+++ src/can_data.py
@@ -12,12 +12,36 @@ def vehicle_speed(data: bytearray) -> int:
     b1 = f"{data[1]:08b}"
     return int(int(b1 + b0, base=2) * speed_mult)
 
+def left_sw_stock(data: bytearray) -> dict[str, int]:
+    b7 = f"{data[7]:08b}"
+
+    new_data = {
+        "left_turn_signal": int(b7[3]),
+        "right_turn_signal": int(b7[2])
+    }
+
+    return new_data
 
 if __name__ == "__main__":
+    # rpm
     array = [0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00]
     data = bytearray(array)
     print(rpm(data))
 
+    # vehicle speed
     array = [0x0F, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
     data = bytearray(array)
     print(vehicle_speed(data))
+
+    # steering wheel left stock
+    array = [0x0F, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00] # everything off
+    data = bytearray(array)
+    print(left_sw_stock(data))
+
+    array = [0x0F, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10] # left turn signal
+    data = bytearray(array)
+    print(left_sw_stock(data))
+
+    array = [0x0F, 0x04, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20] # right turn signal
+    data = bytearray(array)
+    print(left_sw_stock(data))
