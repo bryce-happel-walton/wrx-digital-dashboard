123e2e25bd84ee946c14ea44625eff9262384679
MrTaco9001 <mr.taco9001@gmail.com>
2022-12-02 03:27:05 -0500

    remove false event

--- src/util.py
+++ src/util.py
@@ -1,25 +1,2 @@
 def clamp(low, n, high):
     return min(max(n, low), high)
-
-
-class connection():
-
-    def __init__(self, event, func):
-        self.event = event
-        self.func = func
-
-    def disconnect(self):
-        self.event.connections.pop(self.func)
-
-
-class event():
-
-    connections = {}
-
-    def connect(self, func) -> connection:
-        self.connections[func] = func
-        return connection(self, len(self.connections))
-
-    def emit(self, *args, **kwargs):
-        for v in self.connections:
-            self.connections[v](*args, **kwargs)
