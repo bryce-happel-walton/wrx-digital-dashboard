95c24a2861a5732676fb6839a4a254589724c51a
MrTaco9001 <mr.taco9001@gmail.com>
2022-12-02 16:00:04 -0500

    formatting

--- src/test_provider.py
+++ src/test_provider.py
@@ -24,11 +24,12 @@ def provide_random_message():
     elif key in ["headlights", "handbrake", "reverse_switch", "brake_switch"]:
         data = [0, 0, 0, 0, 0, 0, int(f'0000{randrange(0,2)}000', 2), 0]
     elif key in ["rpm", "neutral_switch"]:
-        data = [0x00, 0x00, 0x00, 0x00, randrange(0, 256), randrange(0, 256), 0x00, 0x00]
+        data = [
+            0x00, 0x00, 0x00, 0x00,
+            randrange(0, 256),
+            randrange(0, 256), 0x00, 0x00
+        ]
     elif key in ["vehicle_speed", "brake_pedal_position"]:
         speed_range_2 = 180 // 14
-        data = [
-            0,
-            randrange(0, speed_range_2 + 1), 0, 0, 0, 0, 0, 0
-        ]
+        data = [0, randrange(0, speed_range_2 + 1), 0, 0, 0, 0, 0, 0]
     return can.Message(arbitration_id=val, data=data)
