c42368422d52da531991199f10fc23c573fdf2b9
MrTaco9001 <22112182+MrTaco9001@users.noreply.github.com>
2022-12-04 03:21:03 -0500

    cruise speed testing

--- src/can_data_parser.py
+++ src/can_data_parser.py
@@ -15,6 +15,9 @@ def vehicle_speed(data: bytearray) -> float:
 
 def cruise_control_speed(data: bytearray) -> float:
     b2 = f"{data[2]:08b}"
+    print(
+        f"{data[0]:08b} {data[1]:08b} {data[2]:08b} {data[3]:08b} {data[4]:08b} {data[5]:08b} {data[6]:08b}", end='\r'
+    )
     return int(b2, base=2) * speed_mult
 
 
