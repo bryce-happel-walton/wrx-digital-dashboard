e252079ef61437138eb88c2d558b4aad7ef95ce3
Bryce <mr.taco9001@gmail.com>
2022-12-16 13:53:49 -0500

    update init script

--- scripts/init_pi.sh
+++ scripts/init_pi.sh
@@ -1,13 +1,13 @@
 cd $HOME
+sudo apt upgrade -y
+sudo apt update -y
+sudo apt install -y build-essential libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
 wget https://www.python.org/ftp/python/3.11.1/Python-3.11.1.tgz
 tar -zxvf Python-3.11.1.tgz
 cd Python-3.11.1
 ./configure --enable-optimizations
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install -y build-essential libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev
 sudo make altinstall
 git clone https://github.com/MrTaco9001/wrx-digital-dashboard.git
 cd $HOME/wrx-digital-dashboard
 git config pull.rebase true
-python3.11 -m pip install -r requirements.txt
+pip3.11 install -r requirements.txt
