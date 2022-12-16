28869add02a3053ff9b1de5b6b6727b696c83b00
Bryce <mr.taco9001@gmail.com>
2022-12-16 16:01:27 -0500

    plan for communication messages

--- src/can_handle.py
+++ src/can_handle.py
@@ -5,14 +5,11 @@ from PyQt5.QtCore import pyqtSignal, pyqtSlot
 from PyQt5.QtWidgets import QApplication, QWidget
 from inspect import getmembers, isfunction
 
-can_ids = {}
 parsers = {x[0]: x[1] for x in getmembers(can_data_parser, isfunction)}
 
 with open("config/can.toml", "rb") as f:
     config = tomllib.load(f)
-
-for i, v in config["can_ids"].items():
-    can_ids[i] = int(v, base=16)
+    can_ids = config["can_ids"]
 
 
 class CanApplication(QWidget):
