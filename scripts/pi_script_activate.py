d7a152033974defe5acb14107a521c17a56a7144
Bryce <mr.taco9001@gmail.com>
2022-12-19 15:13:31 -0500

    fix path

--- scripts/pi_script_activate.py
+++ scripts/pi_script_activate.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     os.mkdir(PATH)
     for file_name in os.listdir("scripts"):
         if ".sh" in file_name:
-            dst = shutil.copyfile(file_name, PATH)
+            dst = shutil.copyfile("scripts"+file_name, PATH)
             st = os.stat(dst)
             os.chmod(dst, st.st_mode | stat.S_IEXEC)
