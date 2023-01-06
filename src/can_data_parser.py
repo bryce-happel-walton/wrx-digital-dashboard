4b8ba64fbcd9a14821bba31fcacc3750e76156d2
MrTaco9001 <22112182+MrTaco9001@users.noreply.github.com>
2023-01-05 22:49:48 -0500

    no reason for these functions to be slots

--- src/can_data_parser.py
+++ src/can_data_parser.py
@@ -1,5 +1,3 @@
-from PyQt5.QtCore import pyqtSlot
-
 LEAST_5_BITS_MASK = (1 << 5) - 1
 LEAST_4_BITS_MASK = (1 << 4) - 1
 SPEED_SCALE = 0.05625
@@ -15,22 +13,18 @@ def is_set(x: int, n: int) -> bool:
     return x & 1 << n != 0
 
 
-@pyqtSlot(bytearray)
 def rpm(data: bytearray) -> int:
     return data[4] + ((data[5] & LEAST_5_BITS_MASK) << 8)
 
 
-@pyqtSlot(bytearray)
 def vehicle_speed(data: bytearray) -> float:
     return (data[0] + (data[1] << 8)) * SPEED_SCALE
 
 
-@pyqtSlot(bytearray)
 def cruise_control_speed(data: bytearray) -> int:
     return data[7]
 
 
-@pyqtSlot(bytearray)
 def turn_signals(data: bytearray) -> list[bool]:
     # * left, right
     new_data = [is_set(data[5], 4), is_set(data[5], 5)]
@@ -38,7 +32,6 @@ def turn_signals(data: bytearray) -> list[bool]:
     return new_data
 
 
-@pyqtSlot(bytearray)
 def fuel_level(data: bytearray) -> float:
     num = data[0] + ((data[1] & LEAST_4_BITS_MASK) << 8)
     num = 1 - (num / 2 - FUEL_LEVEL_MIN) / FUEL_LEVEL_MAX
@@ -46,77 +39,62 @@ def fuel_level(data: bytearray) -> float:
     return num * 100
 
 
-@pyqtSlot(bytearray)
 def oil_temp(data: bytearray) -> int:
     return data[2] + TEMP_SENSOR_OFFSET
 
 
-@pyqtSlot(bytearray)
 def oil_pressure_warning(data: bytearray) -> bool:
     return is_set(data[1], 4)
 
 
-@pyqtSlot(bytearray)
 def coolant_temp(data: bytearray) -> int:
     return data[3] + TEMP_SENSOR_OFFSET
 
 
-@pyqtSlot(bytearray)
 def handbrake_switch(data: bytearray) -> bool:
     return is_set(data[6], 3)
 
 
-@pyqtSlot(bytearray)
 def reverse_switch(data: bytearray) -> bool:
     return is_set(data[6], 2)
 
 
-@pyqtSlot(bytearray)
 def clutch_switch(data: bytearray) -> bool:
     return is_set(data[1], 7)
 
 
-@pyqtSlot(bytearray)
 def tpms_warning(data: bytearray) -> bool:
     return is_set(data[4], 4)
 
 
-@pyqtSlot(bytearray)
 def cruise_control_set(data: bytearray) -> bool:
     return is_set(data[5], 5)
 
 
-@pyqtSlot(bytearray)
 def cruise_control_status(data: bytearray) -> bool:
     return is_set(data[5], 4)
 
 
-@pyqtSlot(bytearray)
 def seatbelt_driver(data: bytearray) -> bool:
     return is_set(data[5], 0)
 
 
-@pyqtSlot(bytearray)
 def dimmer_dial(data: bytearray) -> int:
     return data[0]
 
 
-@pyqtSlot(bytearray)
 def traction_control(data: bytearray) -> bool:
     return is_set(data[1], 3)
 
 
-@pyqtSlot(bytearray)
 def traction_control_mode(data: bytearray) -> bool:
     return is_set(data[0], 3)
 
 
-@pyqtSlot(bytearray)
 def fog_lights(data: bytearray) -> bool:
     return is_set(data[1], 6)
 
 
-@pyqtSlot(bytearray)
 def headlights(data: bytearray) -> list[bool]:
     return [
         is_set(data[7], 3),  # * lowbeams
@@ -126,7 +104,6 @@ def headlights(data: bytearray) -> list[bool]:
     ]
 
 
-@pyqtSlot(bytearray)
 def door_states(data: bytearray) -> list[bool]:
     return [
         is_set(data[1], 0),  # * lf
@@ -137,17 +114,14 @@ def door_states(data: bytearray) -> list[bool]:
     ]
 
 
-@pyqtSlot(bytearray)
 def boost_pressure(data: bytearray) -> float:
     return data[4] * 0.3 - 15.1
 
 
-@pyqtSlot(bytearray)
 def check_engine_light(data: bytearray) -> bool:
     return is_set(data[4], 7)
 
 
-@pyqtSlot(bytearray)
 def gear(data: bytearray) -> int:
     val = data[6] & LEAST_4_BITS_MASK
 
@@ -157,7 +131,6 @@ def gear(data: bytearray) -> int:
     return val
 
 
-@pyqtSlot(bytearray)
 def odometer(data: bytearray) -> float:
     value = 0
 
@@ -167,6 +140,5 @@ def odometer(data: bytearray) -> float:
     return value / 10
 
 
-@pyqtSlot(bytearray)
 def fuel_consumption(data: bytearray) -> float:
     return data[1] * FUEL_CONSUMPTION_SCALE
